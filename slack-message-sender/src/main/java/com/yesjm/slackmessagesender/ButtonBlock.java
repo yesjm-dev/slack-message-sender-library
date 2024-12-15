bb91c56891094700edc1473bd44e581d02486b0e
yesjm <yesjm.dev@gmail.com>
2024-12-15 23:17:15 +0900

    feat: Button Url 유효성 검증 로직 추가

--- slack-message-sender/src/main/java/com/yesjm/slackmessagesender/ButtonBlock.java
+++ slack-message-sender/src/main/java/com/yesjm/slackmessagesender/ButtonBlock.java
@@ -4,11 +4,16 @@ import com.slack.api.model.block.composition.BlockCompositions;
 import com.slack.api.model.block.element.BlockElements;
 import com.slack.api.model.block.element.ButtonElement;
 
+import java.net.URL;
+
 public class ButtonBlock {
     private final String buttonUrl;
     private final String buttonText;
 
     public ButtonBlock(String buttonUrl, String buttonText) {
+        if (!isValidUrl(buttonUrl)) {
+            throw new IllegalArgumentException("Invalid URL provided: " + buttonUrl);
+        }
         this.buttonUrl = buttonUrl;
         this.buttonText = buttonText;
     }
@@ -18,5 +23,14 @@ public class ButtonBlock {
                 .url(buttonUrl)
                 .text(BlockCompositions.plainText(buttonText))
                 .actionId("button"));
+    }
+
+    public static boolean isValidUrl(String url) {
+        try {
+            new URL(url);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
