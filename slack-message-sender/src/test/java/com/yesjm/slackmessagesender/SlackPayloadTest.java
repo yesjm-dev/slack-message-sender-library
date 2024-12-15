fd112384b3ee1832ba3e53cdf1f6e593b0902314
yesjm <yesjm.dev@gmail.com>
2024-12-16 00:48:36 +0900

    test: SlackPayload Test 추가

---
+++ slack-message-sender/src/test/java/com/yesjm/slackmessagesender/SlackPayloadTest.java
@@ -0,0 +1,47 @@
+package com.yesjm.slackmessagesender;
+
+import com.slack.api.model.block.SectionBlock;
+import com.slack.api.webhook.Payload;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SlackPayloadTest {
+
+    @Test
+    void 섹션을_하나_추가하면_블록에_하나의_섹션이_추가된다() {
+        // given
+        SlackPayload.Builder builder = SlackPayload.builder();
+        SectionBlock section = Section.builder()
+                .text("text")
+                .build();
+
+        // when
+        builder.addSection(section);
+        Payload payload = builder.build();
+
+        // then
+        assertNotNull(payload);
+        assertEquals(1, payload.getBlocks().size());
+    }
+
+    @Test
+    void 여러_섹션을_추가하면_블록에_모든_섹션이_추가된다() {
+        // given
+        SlackPayload.Builder builder = SlackPayload.builder();
+        SectionBlock section = Section.builder()
+                .text("text")
+                .build();
+
+        // when
+        builder.addSection(section);
+        builder.addSection(section);
+        builder.addSection(section);
+        Payload payload = builder.build();
+
+        // then
+        assertNotNull(payload);
+        assertEquals(3, payload.getBlocks().size());
+    }
+}
